392bfce88e30a48473f6a941cbe427f940e96f2a
Stephane Humblot <stephane.humblot@etu.univ-grenoble-alpes.fr>
2021-11-08 21:35:32 +0100

    Morpion IA a peu près fonctionnelles (détecte pas les possibilités de gagner dans des cases 'centrales')

--- app/src/main/java/com/example/morpion/Morpion/Game.java
+++ app/src/main/java/com/example/morpion/Morpion/Game.java
@@ -2,6 +2,12 @@ package com.example.morpion.Morpion;
 
 import com.example.morpion.db.Player;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Game {
 
     enum State{Empty, O, X};
@@ -72,6 +78,106 @@ public class Game {
         }
 
         return 0;
+    }
+
+    // check if able to do a winning move -> make a move
+    // check if able to stop a winning move -> make a move
+    // make a random move
+    public ArrayList<Integer> botMoveCoord() {
+        ArrayList<Integer> coord = new ArrayList<>();
+
+        // check if able to win in a row
+        if((coord = checkBotWinningMoveCoord()) != null) return coord;
+
+        if((coord = checkBotBlockingMoveCoord()) != null) return coord;
+
+        return botRandomMoveCoord();
+    }
+
+    public ArrayList<Integer> checkMoveCoord(State s) {
+        ArrayList<Integer> coord = new ArrayList<>();
+
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                if (grid[x][y] != s) break;
+                if (y == n-2 && grid[x][y+1] == State.Empty) {
+                    coord.add(x);
+                    coord.add(y+1);
+                    return coord;
+                };
+            }
+        }
+
+        // check col
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < n; x++) {
+                if (grid[x][y] != s) break;
+                if (x == n-2 && grid[x+1][y] == State.Empty) {
+                    coord.add(x+1);
+                    coord.add(y);
+                    return coord;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            if(grid[i][i] != s) break;
+            if(i == n-2 && grid[i+1][i+1] == State.Empty) {
+                coord.add(i+1);
+                coord.add(i+1);
+                return coord;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if(grid[i][(n-1)-i] == s) break;
+            if(i == n-2 && grid[i+1][i-1] == State.Empty) {
+                coord.add(i+1);
+                coord.add(i-1);
+                return coord;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Integer> checkBotWinningMoveCoord() {
+        // player State
+        State s = playerTurn == 0
+                ? State.O
+                : State.X;
+
+        return  checkMoveCoord(s);
+    }
+
+    public ArrayList<Integer> checkBotBlockingMoveCoord() {
+        // enemy State
+        State s = playerTurn == 0
+                ? State.X
+                : State.O;
+
+        return  checkMoveCoord(s);
+    }
+
+    public ArrayList<Integer> botRandomMoveCoord() {
+        ArrayList<ArrayList<Integer>> randomMoveCoords = new ArrayList<>();
+
+        int i = 0;
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                if (grid[x][y] == State.Empty) {
+                    ArrayList<Integer> coord = new ArrayList<>();
+                    coord.add(0, x);
+                    coord.add(1, y);
+                    randomMoveCoords.add(coord);
+                }
+            }
+        }
+
+        int index = (int)(Math.random() * randomMoveCoords.size());
+
+        return randomMoveCoords.get(index);
+
     }
 
     public void nextPlayer() {
--- app/src/main/java/com/example/morpion/MorpionGameActivity.java
+++ app/src/main/java/com/example/morpion/MorpionGameActivity.java
@@ -25,6 +25,7 @@ import com.example.morpion.db.AppDatabase;
 import com.example.morpion.db.DatabaseClient;
 import com.example.morpion.db.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MorpionGameActivity extends AppCompatActivity {
@@ -33,6 +34,7 @@ public class MorpionGameActivity extends AppCompatActivity {
     private Game game;
     private AppDatabase db;
     private TextView turnView;
+    private LinearLayout grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,9 +54,10 @@ public class MorpionGameActivity extends AppCompatActivity {
         turnView = findViewById(R.id.turn_text);
         turnView.setText("Au tour de " + game.getCurrentPlayer().getName());
 
-        LinearLayout grid = findViewById(R.id.grid_layout);
+        grid = findViewById(R.id.grid_layout);
         for (int i = 0; i < gridSize; i++) {
             LinearLayout row = new LinearLayout(this);
+            row.setTag("" + i);
 
             for (int j = 0; j < gridSize; j++) {
                 ImageView img = new ImageView(this);
@@ -63,11 +66,15 @@ public class MorpionGameActivity extends AppCompatActivity {
                 img.setBackgroundResource(R.drawable.iv_background);
                 LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(gridCellWidth, gridCellWidth);
                 img.setLayoutParams(layoutParams);
-                img.setTag(new String("" + i + j));
+                img.setTag("" + i + j);
                 img.setOnClickListener(this::playerAction);
                 row.addView(img);
             }
             grid.addView(row);
+        }
+
+        if(game.getCurrentPlayer().getId() == 1) {
+            botAction();
         }
     }
 
@@ -86,6 +93,27 @@ public class MorpionGameActivity extends AppCompatActivity {
                 resultAction(res);
                 return;
             }
+            else {
+                game.nextPlayer();
+                turnView.setText("Au tour de " + game.getCurrentPlayer().getName());
+                if(game.getCurrentPlayer().getId() == 1) {
+                    botAction();
+                }
+            }
+        }
+    }
+
+    private void botAction() {
+        ArrayList<Integer> coord = game.botMoveCoord();
+        LinearLayout row = grid.findViewWithTag("" + coord.get(0));
+        ImageView iv = row.findViewWithTag("" + coord.get(0) + coord.get(1));
+        int res = game.move(coord.get(0), coord.get(1));
+        if(res != 0) {
+            resultAction(res);
+            return;
+        }
+        else {
+            setPlayerImage(iv, game.getCurrentPlayer());
             game.nextPlayer();
             turnView.setText("Au tour de " + game.getCurrentPlayer().getName());
         }
